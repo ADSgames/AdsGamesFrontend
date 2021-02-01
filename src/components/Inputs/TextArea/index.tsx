5c1f7ab7d2d7928c405586880a8d92f5d167a373
Allan Legemaate <alegemaate@gmail.com>
2021-02-01 15:05:01 -0500

    Format project

--- src/components/Inputs/TextArea/index.tsx
+++ src/components/Inputs/TextArea/index.tsx
@@ -2,9 +2,18 @@ import * as React from "react";
 import Label from "../Label";
 import { StyledInput } from "./Input.style";
 
-const Button: React.FC<React.HTMLProps<HTMLInputElement> & { label: string }> = ({ label, disabled, required, value, placeholder }) => (<>
-  <Label>{label}</Label>
-  <StyledInput value={value} disabled={disabled} required={required} placeholder={placeholder} />
-</>);
+const Button: React.FC<
+  React.HTMLProps<HTMLInputElement> & { label: string }
+> = ({ label, disabled, required, value, placeholder }) => (
+  <>
+    <Label>{label}</Label>
+    <StyledInput
+      value={value}
+      disabled={disabled}
+      required={required}
+      placeholder={placeholder}
+    />
+  </>
+);
 
 export default Button;
